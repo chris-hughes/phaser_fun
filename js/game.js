236a8ef0487a0426d80b60b4a8bc0fe45532e641
Chris Hughes <hughes.c.i@gmail.com>
2014-02-11 18:37:13 +0000

    simple level created

--- js/game.js
+++ js/game.js
@@ -2,21 +2,29 @@ var game = new Phaser.Game(640, 640, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
 
-	// game.stage.scale.pageAlignHorizontally = true;
-	// game.stage.scale.pageAlignVeritcally = true;
-
+	// background
 	game.load.image('bg', 'assets/PlatformTiles/BackgroundGradient.png');
-	game.load.spritesheet('panda', 'assets/Panda_0.png', 32, 32);
 
+	// level
+	game.load.tilemap('level', 'assets/level.csv', null, Phaser.Tilemap.CSV);
+	game.load.tileset('tiles', 'assets/PlatformTiles/Tiles 32x32/Tiles_32x32.png', 32, 32);
+
+	// player
+	game.load.spritesheet('panda', 'assets/Panda_0.png', 32, 32);
 
 };
 
 function create() {
 
-	
-	game.add.sprite(0, 0, 'bg');
-	// game.stage.backgroundColor = '#787878';
+	// background
+	bg = game.add.sprite(0, 0, 'bg');
 
+	// level
+	map = game.add.tilemap('level');
+	tileset = game.add.tileset('tiles');
+	layer = game.add.tilemapLayer(0, 0, 640, 640, tileset, map, 0);
+
+	// player
 	player = game.add.sprite(32, game.world.height - 150, 'panda');;
 	
 };
