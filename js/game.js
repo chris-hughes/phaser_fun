9844c3450323a29e834f3921368cf64f53917a80
Chris Hughes <hughes.c.i@gmail.com>
2014-02-11 23:06:53 +0000

    snail added

--- js/game.js
+++ js/game.js
@@ -12,6 +12,9 @@ function preload() {
 	// player
 	game.load.spritesheet('panda', 'assets/Panda_0.png', 32, 32);
 
+	// snails
+	game.load.spritesheet('snail', 'assets/snail_3.png', 32, 32);
+
 };
 
 function create() {
@@ -24,17 +27,31 @@ function create() {
 	tileset = game.add.tileset('tiles');
 	// everything collides from all sides (left, right, up, down)
 	tileset.setCollisionRange(0, tileset.tiles.length-1, true, true, true, true);
+	tileset.setCollision(44, false, false, false, true);
 
 	layer = game.add.tilemapLayer(0, 0, 640, 640, tileset, map, 0);
 
 	// player
-	player = game.add.sprite(32, game.world.height - 150, 'panda');
-	player.anchor.setTo(0.5,0.5);
+	player = game.add.sprite(500, game.world.height - 32, 'panda');
+	// player = game.add.sprite(32, game.world.height - 32, 'panda');
+	player.anchor.setTo(0.5,0.5); // used to flip the sprites during animation
 	player.body.gravity.y = 6;
 	player.body.collideWorldBounds = true;
 
 	player.animations.add('left', [15,16,15,17], 500, true);
 	player.animations.add('right', [15,16,15,17], 500, true);
+
+	// snails
+	snail = game.add.sprite(96, game.world.height - 50, 'snail', 14);
+	// snail = game.add.sprite(96, game.world.height - 224, 'snail', 14);
+	snail.body.gravity.y = 6;
+	snail.body.collideWorldBounds = true;
+
+	// snail.animations.add('left', [8,7,8,9], 2000, true);
+	// snail.animations.add('right', [15,14,15,16], 2000, true);
+
+	snail.animations.add('walk', [8,8,8], 2000, true);
+	snail.animations.play('walk');
 
 	// controls
 
@@ -46,6 +63,8 @@ function update(){
 
 	// collisions
 	game.physics.collide(player,layer);
+	game.physics.collide(snail,layer);
+	game.physics.collide(player,snail);
 
 	// player motion
 	player.body.velocity.x = 0;
@@ -53,7 +72,7 @@ function update(){
 	if (cursors.left.isDown) {
         //  Move to the left
         player.body.velocity.x = -150;
-        player.scale.x = -1;
+        player.scale.x = -1; // flip sprite 
         player.animations.play('left');
     }
     else if (cursors.right.isDown){
@@ -69,10 +88,14 @@ function update(){
     }
     
     //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -250;
+    if (cursors.up.isDown && player.body.touching.down){
+        player.body.velocity.y = -300;
+        console.log(snail.x);
     }
 
+    // snail motion
+    // to: function ( properties, duration, ease, autoStart, delay, repeat, yoyo )
+    var tween = game.add.tween(snail).to({ x: 200 }, 2000, Phaser.Easing.Linear.None)
+    .to({ x: 96 }, 2000, Phaser.Easing.Linear.None).loop().start();
 
 };
